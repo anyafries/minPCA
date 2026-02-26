Fix bug in restarts: best score not best variance

minPCA/minpca.py
@@ -1,7 +1,12 @@
 import numpy as np
 import torch
-from minPCA.utils import get_solution, f_maxrcs, f_minpca, f_pca
+from minPCA.utils import get_solution, f_maxrcs, f_minpca, f_regret_variance
 
+BASE_FUNCTIONS = ['minpca', 'maxrcs','maxregret']
+DEVELOPMENT_FUNCTIONS = [
+    'seq', 'pooled', 'average', #'fairpca', 
+    'minpca_pen', 'regret_reconstruction'
+]
 
 def _to_torch(Xs):
     """Convert list of numpy arrays to list of torch tensors."""
@@ -36,6 +41,18 @@ def get_vars_pca(v, params=None, params_pooled=None):
         pooled = -f_minpca(v, params_pooled['covs'], 
                            params_pooled['norm_csts']).detach().item()
     return max_training, pooled
+
+
+def _get_score(v, params, function):
+    if function == 'minpca':
+        return -get_vars_pca(v, params)[0]
+    elif function == 'maxrcs':
+        return get_errs_pca(v, params, from_cov=True)[0]
+    elif function == 'maxregret':
+        return f_regret_variance(
+            v, params['covs'], params['norm_csts'], params['opt_vals']
+        ).detach().item()
+    return None
 
 
 def generate_params(Xs, pooled=False, norm=True, from_cov=False, center=True):
@@ -137,14 +154,9 @@ class minPCA():
     def __init__(self, n_components, function='minpca', norm=True):
         # Initial checks
         assert isinstance(n_components, int), "`n_components` should be an integer."
-        base_functions = ['minpca', 'maxrcs','maxregret']
-        development_functions = [
-            'seq', 'pooled', 'average', #'fairpca', 
-            'minpca_pen', 'regret_reconstruction'
-        ]
-        assert function in base_functions + development_functions, \
+        assert function in BASE_FUNCTIONS + DEVELOPMENT_FUNCTIONS, \
             f"function '{function}' not recognized."
-        if function in development_functions:
+        if function in DEVELOPMENT_FUNCTIONS:
             print(f"Warning: function '{function}' is in development mode.")
         assert isinstance(norm, bool), "`norm` should be a boolean."
 
@@ -156,18 +168,30 @@ class minPCA():
             n_restarts=5, verbose=False, v0=None):
         assert isinstance(Xs, list)
         assert len(set(X.shape[1] for X in Xs)) == 1
-        Xs = _to_torch(Xs)
+        if self.function_ in DEVELOPMENT_FUNCTIONS and n_restarts > 1:
+            print(f"Warning: n_restarts > 1 is not implemented for function '{self.function_}'.")
+            print(f"Setting n_restarts to 1.")
+            n_restarts = 1
 
+        Xs = _to_torch(Xs)
         self.K_ = len(Xs)
         self.p_ = Xs[0].shape[1]
         self.Xs, self.means_, self.pooled_mean_ = None, None, None
-
         self.params_ = generate_params(Xs, norm=self.norm_, from_cov=True)
         self.params_pooled_ = generate_params(Xs, pooled=True, norm=self.norm_,
                                               from_cov=True)
         
+        # initialize v0
+        if v0 is None: 
+            v0 = torch.randn(self.p_, self.n_components_)
+        else:
+            if isinstance(v0, np.ndarray):
+                v0 = torch.from_numpy(v0).float()
+            assert v0.shape == (self.p_, self.n_components_), \
+                f"v0 should have shape ({self.p_}, {self.n_components_}), but got {v0.shape}."
+            
+        # for the regret: need the optimal variance/error per environemnt
         if self.function_ in ['maxregret', 'regret_reconstruction']:
-            # compute the optimal variances / errors for each environment
             opt_vals = []
             for cov, norm_cst in zip(self.params_['covs'], self.params_['norm_csts']):
                 eigvals, _ = torch.linalg.eig(cov / norm_cst)
@@ -179,27 +203,22 @@ class minPCA():
                     opt_vals.append(torch.sum(eigvals[self.n_components_:]).item())
             self.params_['opt_vals'] = opt_vals
 
-        if v0 is None: 
-            v0 = torch.randn(self.p_, self.n_components_)
-        else:
-            if isinstance(v0, np.ndarray):
-                v0 = torch.from_numpy(v0).float()
-            assert v0.shape == (self.p_, self.n_components_), \
-                f"v0 should have shape ({self.p_}, {self.n_components_}), but got {v0.shape}."
+        
         v0 = get_solution(v0, self.params_, function=self.function_, c=0.99, 
                                rank=self.n_components_, n_iters=n_iters,
                                lr=lr, betas=betas, method=method)
-        best_v, best_var = v0, get_vars_pca(v0, self.params_)[0]
+        best_v = v0
         if n_restarts > 1:
+            best_score = _get_score(v0, self.params_, self.function_)
             for _ in range(n_restarts - 1):
                 v_try = torch.randn(self.p_, self.n_components_)
                 v_try = get_solution(v_try, self.params_, function=self.function_, c=0.99, 
                                   rank=self.n_components_, n_iters=n_iters,
                                   lr=lr, betas=betas, method=method)
-                try_var = get_vars_pca(v_try, self.params_)[0]
-                if best_var < try_var:
-                    if verbose: print(f"\timproving from {best_var:.3f} to {try_var:.3f}")
-                    best_v, best_var = v_try, try_var
+                score_try = _get_score(v_try, self.params_, self.function_)
+                if score_try < best_score:
+                    if verbose: print(f"\timproving from {best_score:.3f} to {score_try:.3f}")
+                    best_v, best_score = v_try, score_try
         self.v_ = best_v
 
         self.maxerr_, self.pooled_err_ = get_errs_pca(